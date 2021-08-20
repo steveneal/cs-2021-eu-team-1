f4618682130a48bfac90b485e92e6ca6ebddd9d4
henrik <hans.henrik.lehmann@gmail.com>
2021-08-20 19:10:21 +0100

    Add test for InstrumentLiquidityExtractor

---
+++ src/test/java/com/cs/rfq/decorator/extractors/AverageTradedPriceExtractorTest.java
@@ -0,0 +1,44 @@
+package com.cs.rfq.decorator.extractors;
+
+import com.cs.rfq.decorator.Rfq;
+import com.cs.rfq.decorator.TradeDataLoader;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class AverageTradedPriceExtractorTest extends AbstractSparkUnitTest {
+
+
+    private Rfq rfq;
+    Dataset<Row> trades;
+
+    @BeforeEach
+    public void setup() {
+        rfq = new Rfq();
+        rfq.setIsin("AT0000A0VRQ6");
+
+        String filePath = getClass().getResource("volume-traded-3.json").getPath();
+        trades = new TradeDataLoader().loadTrades(session, filePath);
+    }
+
+
+    @Test
+    public void testCorrectVolumeExtractedGivenInstrument() {
+        InstrumentLiquidityExtractor instLiquidExtractor = new InstrumentLiquidityExtractor();
+
+        instLiquidExtractor.setSince("2021-08-01");
+        Map<RfqMetadataFieldNames, Object> map = instLiquidExtractor.extractMetaData(rfq, session, trades);
+        Object result = map.get(RfqMetadataFieldNames.totalTradesLiquidity);
+        //results.put(RfqMetadataFieldNames.totalTradesLiquidity, volume);
+
+        assertEquals(850_000L, result);
+    }
+
+}
+
